a32ac53b08e1b5e559ad7409843c206849a7080b
francorosa <km115.franco@gmail.com>
2022-03-17 19:26:16 -0500

    Add array format

--- src/components/AddCard.jsx
+++ src/components/AddCard.jsx
@@ -1,19 +1,19 @@
-import { connect } from "react-redux"
+import { connect } from "react-redux";
 import { addToVideoSets } from "../actions";
 
 const AddCard = ({ addToVideoSets }) => (
   <a className="panel-block" onClick={addToVideoSets}>
     <span class="icon-text">
-      <span class="icon has-text-link">
+      <span class="icon has-text-link mr-1">
         <i class="fas fa-plus"></i>
       </span>
       <span>Añadir paradero</span>
     </span>
   </a>
-)
+);
 
-const mapDispatchToProps = dispatch => ({
-  addToVideoSets: () => dispatch(addToVideoSets())
-}) 
+const mapDispatchToProps = (dispatch) => ({
+  addToVideoSets: () => dispatch(addToVideoSets()),
+});
 
 export default connect(null, mapDispatchToProps)(AddCard);
--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,68 +1,107 @@
-import { useEffect } from "react";
-import { connect } from "react-redux"
-
-
+import { connect } from "react-redux";
 
 const Download = ({ videosets }) => {
   let result;
-  
+
   const generateFile = () => {
-    result = videosets.map(videoset => ({
+    result = videosets.map((videoset) => ({
       nb: videoset.title,
       la: String(videoset.lat),
       lo: String(videoset.lng),
       na: videoset.file[1],
-      ru: videoset.angle>=0 ? String(videoset.angle) : String(parseFloat(videoset.angle)+360),
-    }))
+      ru:
+        videoset.angle >= 0
+          ? String(videoset.angle)
+          : String(parseFloat(videoset.angle) + 360),
+    }));
     let currentTime = new Date();
-    let currentStr = currentTime.toLocaleString('sv-SE').split(/-|:/).join('');
-    let filePrefix = currentStr.replace(' ','_') 
-    let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(result));
-    let dlAnchorElem = document.querySelector('.download_compact');
-    dlAnchorElem.setAttribute("href",     dataStr     );
+    let currentStr = currentTime.toLocaleString("sv-SE").split(/-|:/).join("");
+    let filePrefix = currentStr.replace(" ", "_");
+    let dataStr =
+      "data:text/json;charset=utf-8," +
+      encodeURIComponent(JSON.stringify(result));
+    let dlAnchorElem = document.querySelector(".download_compact");
+    dlAnchorElem.setAttribute("href", dataStr);
     dlAnchorElem.setAttribute("download", `${filePrefix}_compacto.txt`);
-  }
+  };
 
   const generateFileNormal = () => {
-    result = videosets.map(videoset => ({
+    result = videosets.map((videoset) => ({
       nb: videoset.title,
       la: String(videoset.lat),
       lo: String(videoset.lng),
-      na: videoset.angle>=0 ? String(videoset.angle) : String(parseFloat(videoset.angle)+360),
+      na:
+        videoset.angle >= 0
+          ? String(videoset.angle)
+          : String(parseFloat(videoset.angle) + 360),
       ru: videoset.file[1],
-    }))
+    }));
     let currentTime = new Date();
-    let currentStr = currentTime.toLocaleString('sv-SE').split(/-|:/).join('');
-    let filePrefix = currentStr.replace(' ','_') 
-    let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(result, null, 2));
-    let dlAnchorElem = document.querySelector('.download_normal');
-    dlAnchorElem.setAttribute("href",     dataStr     );
+    let currentStr = currentTime.toLocaleString("sv-SE").split(/-|:/).join("");
+    let filePrefix = currentStr.replace(" ", "_");
+    let dataStr =
+      "data:text/json;charset=utf-8," +
+      encodeURIComponent(JSON.stringify(result, null, 2));
+    let dlAnchorElem = document.querySelector(".download_normal");
+    dlAnchorElem.setAttribute("href", dataStr);
     dlAnchorElem.setAttribute("download", `${filePrefix}_normal.txt`);
-  }
+  };
+
+  const formatAngle = (angle) => {
+    let ang = parseFloat(angle);
+    return ang >= 0 ? ang : ang + 360;
+  };
+
+  const generateFileArray = () => {
+    result = videosets.map((videoset) => [
+      `${Math.abs(videoset.lat).toFixed(5)}, ${Math.abs(videoset.lng).toFixed(
+        5
+      )}, ${formatAngle(videoset.angle).toFixed(2)},`,
+    ]);
+    console.log(result);
+    let currentTime = new Date();
+    let currentStr = currentTime.toLocaleString("sv-SE").split(/-|:/).join("");
+    let filePrefix = currentStr.replace(" ", "_");
+    console.log(result.join("\n"));
+    let dataStr =
+      "data:text/json;charset=utf-8," + encodeURIComponent(result.join("\n"));
+    let dlAnchorElem = document.querySelector(".download_array");
+    dlAnchorElem.setAttribute("href", dataStr);
+    dlAnchorElem.setAttribute("download", `${filePrefix}_array.txt`);
+  };
 
   return (
     <>
       <a href=""></a>
       <a className="panel-block download_compact" onClick={generateFile}>
-        <span class="icon-text">
-          <span class="icon has-text-link">
-            <i class="fas fa-download"></i>
+        <span className="icon-text">
+          <span className="icon has-text-link mr-1">
+            <i className="fas fa-download"></i>
           </span>
           <span>Descargar Compacto</span>
         </span>
       </a>
       <a className="panel-block download_normal" onClick={generateFileNormal}>
-      <span class="icon-text">
-        <span class="icon has-text-link">
-          <i class="fas fa-file-download"></i>
+        <span className="icon-text">
+          <span className="icon has-text-link mr-1">
+            <i className="fas fa-file-download"></i>
+          </span>
+          <span>Descargar Normal</span>
         </span>
-        <span>Descargar Normal</span>
-      </span>
-    </a>
-  </>
-)}
+      </a>
+      <a className="panel-block download_array" onClick={generateFileArray}>
+        <span className="icon-text">
+          <span className="icon has-text-link mr-1">
+            <i className="fas fa-code"></i>
+          </span>
+          <span>Descargar Array</span>
+        </span>
+      </a>
+    </>
+  );
+};
 
-const mapStateToProps = state => ({
-  videosets: state.videosets
-}) 
+const mapStateToProps = (state) => ({
+  videosets: state.videosets,
+});
 export default connect(mapStateToProps)(Download);
